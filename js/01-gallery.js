98c446b3d4b9664e60bc77d6f75d6491a9d21907
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-19 10:44:03 +0300

    galley start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup(images) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    }).join('');
+};
+
+// console.log(createGalleryMarkup(galleryItems))
+
+const galleryItemsMarkup = createGalleryMarkup(galleryItems);
+console.log(galleryItemsMarkup);
+
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+
